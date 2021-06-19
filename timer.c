fd30e1d982f91924496756c4d41fd7a282a852a6
Hung <viethunghoang1999@gmail.com>
2021-06-19 15:37:35 +0700

    timer for read button

--- timer.c
+++ timer.c
@@ -1,4 +1,5 @@
 #include"timer.h"
+#include "type.h"
 void init_timer_interrupt_MS(uint32_t tim)//1ms-1000ms
 {
 	TIM_TimeBaseInitTypeDef TIM;
@@ -17,31 +18,11 @@ void init_timer_interrupt_MS(uint32_t tim)//1ms-1000ms
 	//config interrupt
 	nvic.NVIC_IRQChannel=TIM4_IRQn;
 	nvic.NVIC_IRQChannelCmd=ENABLE;
-	nvic.NVIC_IRQChannelPreemptionPriority=1;
+	nvic.NVIC_IRQChannelPreemptionPriority=TIMER_SET_HANDSHARK;
 	nvic.NVIC_IRQChannelSubPriority=0;
 	NVIC_Init(&nvic);
 }
-void init_timer_interrupt_US(uint32_t tim)//1us-100us
-{
-	TIM_TimeBaseInitTypeDef TIM;
-	RCC_ClocksTypeDef ClksFreq;
-	NVIC_InitTypeDef nvic;
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2,ENABLE);
-	TIM.TIM_CounterMode=TIM_CounterMode_Up;	
-	uint32_t period=72*tim-1;
-	TIM.TIM_Period=period;
-	TIM.TIM_Prescaler=0;
-	TIM_TimeBaseInit(TIM_channel_button,&TIM);
-	TIM_ClearFlag(TIM_channel_button,TIM_FLAG_Update);
-	TIM_ITConfig(TIM_channel_button,TIM_IT_Update,ENABLE);
-	TIM_Cmd(TIM_channel_button,ENABLE);
-	//config interrupt
-	nvic.NVIC_IRQChannel=TIM2_IRQn;
-	nvic.NVIC_IRQChannelCmd=ENABLE;
-	nvic.NVIC_IRQChannelPreemptionPriority=1;
-	nvic.NVIC_IRQChannelSubPriority=0;
-	NVIC_Init(&nvic);
-}
+
 void init_timer_read_button (uint32_t t){
 	TIM_TimeBaseInitTypeDef TIM;
 	RCC_ClocksTypeDef ClksFreq;
@@ -59,7 +40,7 @@ void init_timer_read_button (uint32_t t){
 	//config interrupt
 	nvic.NVIC_IRQChannel=TIM2_IRQn;
 	nvic.NVIC_IRQChannelCmd=ENABLE;
-	nvic.NVIC_IRQChannelPreemptionPriority=1;
-	nvic.NVIC_IRQChannelSubPriority=1;
+	nvic.NVIC_IRQChannelPreemptionPriority=TIMER_READ_BUTTON;
+	nvic.NVIC_IRQChannelSubPriority=0;
 	NVIC_Init(&nvic);
 }
